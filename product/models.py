a9c0a9d8bb63683e417d0f7aa1a5bd369048c590
atheby <atheby.the@gmail.com>
2017-11-02 21:31:09 +0100

    upload image path change

--- product/models.py
+++ product/models.py
@@ -6,7 +6,7 @@ from django.core.validators import MinValueValidator
 class Product(models.Model):
     name = models.CharField(max_length=200)
     description = models.TextField()
-    image = models.ImageField(upload_to='media/products', blank=True)
+    image = models.ImageField(upload_to='products', blank=True)
     price = models.DecimalField(max_digits=6, decimal_places=2, validators=[MinValueValidator(Decimal('0.01'))])
     quantity = models.DecimalField(max_digits=4, decimal_places=0, validators=[MinValueValidator(Decimal('0'))])
     visible = models.BooleanField(default=False)
